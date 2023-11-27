f127d5cd32698a9359eeeb1441f18ceedf05a008
Abu Zharr Luqman bin Abdul Aziz <32317099+panjang007@users.noreply.github.com>
2023-11-27 22:56:33 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/example/mysejahtera/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mysejahtera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(myApp());
+}
+
+void test() {
+  List<String>? names = ['Foo'];
+  print(names);
+}
+
+class myApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
